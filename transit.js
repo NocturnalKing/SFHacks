31e13a718ea23e2ce7caf7b472ee07c1d71d34f3
E.K <fake.hack376@gmail.com>
2024-04-07 03:55:06 -0700

    Updated api request

--- transit.js
+++ transit.js
@@ -3,7 +3,7 @@ exports.XMLHttpRequest = XMLHttpRequest;
 
 let request = new XMLHttpRequest();
 
-request.open('POST', "https://api.openrouteservice.org/v2/directions/driving-car");
+request.open('POST', "https://api.openrouteservice.org/v2/directions/driving-car?Authorization=5b3ce3597851110001cf624860a17d1d27434e038a437dd74419af40&coordinates=[[8.681495,49.41461],[8.686507,49.41943],[8.687872,49.420318]]&units=mi");
 
 
 request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
