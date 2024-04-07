52c5c61e3cadcfb0de47b6c72cf793b067576006
E.K <fake.hack376@gmail.com>
2024-04-06 17:50:15 -0700

    Testing an error fix

--- transit.js
+++ transit.js
@@ -1,3 +1,5 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 var request = new XMLHttpsRequest();
 
 request.open('GET', 'https://api.openrouteservice.org?api_key=5b3ce3597851110001cf624860a17d1d27434e038a437dd74419af40&start=8.681495,49.41461&end=8.687872,49.420318')
