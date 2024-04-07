9cb2cb186ecd24a6f4d3d25ac5b989f0f8626805
E.K <fake.hack376@gmail.com>
2024-04-06 20:38:44 -0700

    Used a new example for the transit.js file

--- transit.js
+++ transit.js
@@ -1,18 +1,23 @@
-const { request } = require("./request");
-
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 exports.XMLHttpRequest = XMLHttpRequest;
 
-request.open('GET', 'https://api.openrouteservice.org?api_key=5b3ce3597851110001cf624860a17d1d27434e038a437dd74419af40&start=8.681495,49.41461&end=8.687872,49.420318')
+let request = new XMLHttpRequest();
+
+request.open('POST', "https://api.openrouteservice.org/v2/directions/driving-car");
+
 
 request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
+request.setRequestHeader('Content-Type', 'application/json');
+request.setRequestHeader('Authorization', '5b3ce3597851110001cf624860a17d1d27434e038a437dd74419af40');
 
 request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-        console.log('Status', this.status);
-        console.log('Headers', this.getAllResponseHeader)
-        console.log('Body', this.responseText)
-    }
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
 };
 
-request.send();
+const body = '{"coordinates":[[8.681495,49.41461],[8.686507,49.41943],[8.687872,49.420318]],"maneuvers":"false"}';
+
+request.send(body);
